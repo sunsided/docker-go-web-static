f26603f5be6db9580b1beb28b6647097a9cb5f61
Markus Mayer <widemeadows@gmail.com>
2015-07-17 01:48:11 +0200

    initial commit

---
+++ main.go
@@ -0,0 +1,25 @@
+package main // import "minefield/go-webget"
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+)
+
+func main() {
+	response, err := http.Get("http://httpstat.us/200")
+	check(err)
+
+	body, err := ioutil.ReadAll(response.Body)
+	check(err)
+
+	fmt.Printf("%s\n", string(body))
+}
+
+func check(err error) {
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
